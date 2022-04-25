89c72713aa9ecee880de4d82ad514b87c49b19dd
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2022-04-25 21:18:03 +1000

    Fix linting offense

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -76,7 +76,7 @@ var builtins = map[string]*object.Builtin{
 			arr := args[0].(*object.Array)
 			length := len(arr.Elements)
 			if length > 0 {
-				newElements := make([]object.Object, length-1, length-1)
+				newElements := make([]object.Object, length-1)
 				copy(newElements, arr.Elements[1:length])
 				return &object.Array{Elements: newElements}
 			}
