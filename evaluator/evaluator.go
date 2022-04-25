c044bbe0d58f8d84d88e229e6fb0758775fecefd
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2022-04-25 16:40:01 +1000

    Add evaluation of identifiers (variables)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -41,6 +41,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 		env.Set(node.Name.Value, val)
 
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
+
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
@@ -198,6 +201,15 @@ func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Obje
 	} else {
 		return NULL
 	}
+}
+
+func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return newError("identifier not found: " + node.Value)
+	}
+
+	return val
 }
 
 func isTruthy(obj object.Object) bool {
