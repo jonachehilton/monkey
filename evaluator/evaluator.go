0806ca575840539fca934cdad9407b3369df7fc3
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2022-04-25 15:58:22 +1000

    Add evaluation of return statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,11 +16,11 @@ func Eval(node ast.Node) object.Object {
 
 	// Statements
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -36,16 +36,37 @@ func Eval(node ast.Node) object.Object {
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	}
 
 	return nil
 }
 
-func evalStatements(stms []ast.Statement) object.Object {
+func evalProgram(stms []ast.Statement) object.Object {
 	var result object.Object
 
 	for _, statement := range stms {
 		result = Eval(statement)
+
+		if returnVale, ok := result.(*object.ReturnValue); ok {
+			return returnVale.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
 	}
 
 	return result
