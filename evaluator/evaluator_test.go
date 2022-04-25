4bec731b4bfb0bb492478fdbc6f55cdb37300d20
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2022-04-25 17:50:13 +1000

    Add testing for evaluating string concating

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -161,6 +161,7 @@ func TestErrorHandling(t *testing.T) {
 			return 1;
 			}`, "unknown operator: BOOLEAN + BOOLEAN"},
 		{"foobar", "identifier not found: foobar"},
+		{`"Hello" - "World"`, "unknown operator: STRING - STRING"},
 	}
 
 	for _, tt := range tests {
@@ -246,6 +247,20 @@ func TestClosures(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
